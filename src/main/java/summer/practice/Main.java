d2775e8ccb611dff271d6a34223c1cce3bf42bb4
Ord00 <ivansimonov7@yandex.ru>
2025-07-09 17:39:34 +0300

    Added description

--- src/main/java/summer/practice/Main.java
+++ src/main/java/summer/practice/Main.java
@@ -3,9 +3,15 @@ package summer.practice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Симонов Иван Алексеевич 3 курс 91 группа ПММ
+// Приложение получает на вход строку текста, написанного на языке SQL.
+// После этого выполняет лексический анализ введённого запроса,
+// т.е. разбивает предложение на слова, каждому из которых ставит в соответствие категорию.
+// Если слово распознать не удаётся, то выводится информация об ошибке.
+// Распознаватель построен на основе конечных автоматов (FSM).
+// В результате работы получается поток токенов - пар лексема-категория
 @SpringBootApplication
 public class Main {
-
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
