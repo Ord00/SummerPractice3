6cf354072c500f0e8c6e91e0101044f0659c3d67
Ord00 <ivansimonov7@yandex.ru>
2025-07-15 23:40:06 +0300

    Divided into modules and added maven

---
+++ Web/src/main/java/summer/practice/dto/SqlRequest.java
@@ -0,0 +1,13 @@
+package summer.practice.dto;
+
+public class SqlRequest {
+    private String sqlQuery;
+
+    public String getSqlQuery() {
+        return sqlQuery;
+    }
+
+    public void setSqlQuery(String sqlQuery) {
+        this.sqlQuery = sqlQuery;
+    }
+}
--- src/main/java/summer/practice/service/exceptions/SQLScanException.java
+++
@@ -1,7 +0,0 @@
-package summer.practice.service.exceptions;
-
-public class SQLScanException extends Exception {
-    public SQLScanException(String message) {
-        super(message);
-    }
-}
